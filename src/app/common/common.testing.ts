9b41a92100e45189c1afe85dee0b10532732e13e
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-12-21 17:35:27 +0200

    implement common testing module

---
+++ src/app/common/common.testing.ts
@@ -0,0 +1,37 @@
+import { MediaChange } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Observable, of, Subscription } from 'rxjs';
+import { MaterialModule } from '../material.module';
+import { RouterTestingModule } from '@angular/router/testing';
+
+export class MediaObserverFake {
+  isActive(query: string): boolean {
+    return false;
+  }
+
+  asObservable(): Observable<MediaChange> {
+    return of({} as MediaChange);
+  }
+
+  subscribe(
+    next?: (value: MediaChange) => void,
+    error?: (error: any) => void,
+    complete?: () => void
+  ): Subscription {
+    return new Subscription();
+  }
+}
+
+export const commonTestingProviders: any[] = [
+  // Intentionally Left Blank!!!
+];
+
+export const commonTestingModules: any[] = [
+  ReactiveFormsModule,
+  MaterialModule,
+  NoopAnimationsModule,
+  HttpClientTestingModule,
+  RouterTestingModule,
+];
