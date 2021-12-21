074c9dc64ad6e040a63a953bd0f6f55d46a63019
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-12-21 18:05:29 +0200

    create user model & interface

---
+++ src/app/user/user.ts
@@ -0,0 +1,90 @@
+import { Role } from '../auth/auth.enum';
+
+export interface IName {
+  first: string;
+  middle?: string;
+  last: string;
+}
+
+export enum IPhoneType {
+  None = 'none',
+  Mobile = 'mobile',
+  Home = 'home',
+  Work = 'work',
+}
+
+export interface IPhone {
+  type: IPhoneType;
+  digits: string;
+  id: number;
+}
+
+export interface IUser {
+  id: number;
+  email: string;
+  name: IName;
+  picture: string;
+  role: Role | string;
+  userStatus: boolean;
+  dateOfBirth?: Date;
+  level: number;
+  address: {
+    line1: string;
+    line2?: string;
+    city: string;
+    state: string;
+    zip: string;
+  };
+  phones: IPhone[];
+  readonly fullName: string;
+}
+
+export class User implements IUser {
+  constructor(
+    public id = 0,
+    public email = '',
+    public name = { first: '', middle: '', last: '' } as IName,
+    public picture = '',
+    public role = Role.None,
+    public dateOfBirth = new Date(),
+    public userStatus = false,
+    public level = 0,
+    public address = {
+      line1: '',
+      city: '',
+      state: '',
+      zip: '',
+    },
+    public phones: IPhone[] = []
+  ) {}
+
+  static Build(user: IUser) {
+    if (!user) {
+      return new User();
+    }
+
+    return new User(
+      user.id,
+      user.email,
+      user.name,
+      user.picture,
+      user.role as Role,
+      user.dateOfBirth,
+      user.userStatus,
+      user.level,
+      user.address,
+      user.phones
+    );
+  }
+
+  get fullName(): string {
+    return `${this.name.first} ${this.name.middle} ${this.name.last}`;
+  }
+
+  toJSON(): object {
+    const serialized = Object.assign(this);
+    delete serialized.id;
+    delete serialized.fullName;
+    return serialized;
+  }
+}
