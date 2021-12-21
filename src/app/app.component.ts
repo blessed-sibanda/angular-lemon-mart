98ce0d4438633a5441507cd1284ac93dc3f0d775
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-12-21 15:24:36 +0200

    improve toolbar and home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,17 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <mat-toolbar color="primary">
+      <button mat-icon-button><mat-icon>menu</mat-icon></button>
       <a mat-button routerLink="/home"><h1>LemonMart</h1></a>
+      <span class="flex-spacer"></span>
+      <button mat-icon-button>
+        <mat-icon>account_circle</mat-icon>
+      </button>
+      <button mat-icon-button>
+        <mat-icon>lock_open</mat-icon>
+      </button>
     </mat-toolbar>
+    <router-outlet></router-outlet>
   `,
   styleUrls: ['./app.component.scss'],
 })
