0b8e0543a43cb5e2aadde2f9778aa0e5ee029670
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-12-22 08:53:48 +0200

    implement auth http interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AuthService } from './auth/auth.service';
 import { InMemoryAuthService } from './auth/in-memory-auth.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthHttpInterceptor } from './auth/auth-http-interceptor';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, PageNotFoundComponent],
@@ -20,7 +22,14 @@ import { InMemoryAuthService } from './auth/in-memory-auth.service';
     MaterialModule,
     FlexLayoutModule,
   ],
-  providers: [{ provide: AuthService, useClass: InMemoryAuthService }],
+  providers: [
+    { provide: AuthService, useClass: InMemoryAuthService },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthHttpInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
